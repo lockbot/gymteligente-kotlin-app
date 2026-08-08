103ae08e12156fcef0f7b5152f12d0cf77f9dd92
lockbot <mail.keybot@gmail.com>
2026-08-07 23:51:12 -0300

    feat: learning kotlin kotlin counter, adding activity

--- app/src/main/java/com/example/gymteligente/MainActivity.kt
+++ app/src/main/java/com/example/gymteligente/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.gymteligente
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        val textCounter = findViewById<TextView>(R.id.text_counter)
+        val btnIncrease = findViewById<Button>(R.id.btn_increase)
+        val btnDecrease = findViewById<Button>(R.id.btn_decrease)
+        val btnReset = findViewById<Button>(R.id.btn_reset)
+
+
+        btnIncrease.setOnClickListener {
+            counter++
+            textCounter.text = counter.toString()
+        }
+        btnDecrease.setOnClickListener {
+            if (counter > 0){
+                counter--
+            }
+            textCounter.text = counter.toString()
+        }
+        btnReset.setOnClickListener {
+            counter = 0
+            textCounter.text = counter.toString()
+        }
     }
 }
