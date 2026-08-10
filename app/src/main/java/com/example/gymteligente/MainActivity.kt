5599db3ebcba77088f2091ee87451d1b91e104bf
lockbot <mail.keybot@gmail.com>
2026-08-10 09:06:50 -0300

    feat: activity binding

--- app/src/main/java/com/example/gymteligente/MainActivity.kt
+++ app/src/main/java/com/example/gymteligente/MainActivity.kt
@@ -7,32 +7,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gymteligente.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private var counter = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textCounter = findViewById<TextView>(R.id.text_counter)
-        val btnIncrease = findViewById<Button>(R.id.btn_increase)
-        val btnDecrease = findViewById<Button>(R.id.btn_decrease)
-        val btnReset = findViewById<Button>(R.id.btn_reset)
+//        setContentView(R.layout.activity_main)
+
+//        val textCounter = findViewById<TextView>(R.id.text_counter)
+//        val btnIncrease = findViewById<Button>(R.id.btn_increase)
+//        val btnDecrease = findViewById<Button>(R.id.btn_decrease)
+//        val btnReset = findViewById<Button>(R.id.btn_reset)
 
 
-        btnIncrease.setOnClickListener {
+        binding.btnIncrease.setOnClickListener {
             counter++
-            textCounter.text = counter.toString()
+            binding.textCounter.text = counter.toString()
         }
-        btnDecrease.setOnClickListener {
+        binding.btnDecrease.setOnClickListener {
             if (counter > 0){
                 counter--
             }
-            textCounter.text = counter.toString()
+            binding.textCounter.text = counter.toString()
         }
-        btnReset.setOnClickListener {
+        binding.btnReset.setOnClickListener {
             counter = 0
-            textCounter.text = counter.toString()
+            binding.textCounter.text = counter.toString()
         }
     }
 }
